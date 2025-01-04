7a732a584c34e5649866e7c5191d78c9dce85fd5
leonardo.mendes <leodsmf@gmail.com>
2025-01-04 14:54:48 -0300

    configurando vitest com SWC

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { PrismaService } from './prisma/prisma.service'
-import { envSchema } from 'src/env'
+import { envSchema } from '@/env'
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
