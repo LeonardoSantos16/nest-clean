42dc7ff09e4b5ae607291985004060e6d20aa9cf
leonardo <leodsmf@gmail.com>
2025-01-06 12:45:30 -0300

    Presenter de perguntas

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { QuestionPresenter } from '../presenters/question-presenter'
 import { z } from 'zod'
 const pageQueryParamSchema = z
   .string()
@@ -17,9 +18,13 @@ export class FetchRecentQuestionsController {
   constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const questions = await this.fetchRecentQuestions.execute({
+    const result = await this.fetchRecentQuestions.execute({
       page,
     })
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+    const questions = result.value.questions
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }
