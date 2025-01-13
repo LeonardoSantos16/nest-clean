57265aff9282c8e996dc665505ee58321286298a
leonardo <leodsmf@gmail.com>
2025-01-13 16:00:03 -0300

    Controller: Listar respostas da pergunta

--- src/domain/forum/application/use-cases/fetch-question-answers.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -1,7 +1,7 @@
 import { Either, right } from '@/core/either'
 import { AnswersRepository } from '../repositories//answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
-
+import { Injectable } from '@nestjs/common'
 interface FetchQuestionAnswersUseCaseRequest {
   questionId: string
   page: number
@@ -14,6 +14,7 @@ type FetchQuestionAnswersUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class FetchQuestionAnswersUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
