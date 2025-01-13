77bb6ac3646cb6ee4e9e7feed184be648cd3a7d4
leonardo.mendes <leodsmf@gmail.com>
2025-01-13 18:59:53 -0300

    uploads - Testando controller de upload

--- src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -28,5 +28,8 @@ describe('Upload attachment (E2E)', () => {
       .set('Authorization', `Bearer ${accessToken}`)
       .attach('file', './test/e2e/sample-upload.png')
     expect(response.statusCode).toBe(201)
+    expect(response.body).toEqual({
+      attachmentId: expect.any(String),
+    })
   })
 })
