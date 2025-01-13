a58c7e7b3f3e694d7f58b7d8759e52da0162f062
leonardo.mendes <leodsmf@gmail.com>
2025-01-13 19:37:13 -0300

    Criação de pergunta com anexos

--- src/domain/forum/enterprise/entities/attachment.ts
+++ src/domain/forum/enterprise/entities/attachment.ts
@@ -1,6 +1,6 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-interface AttachmentProps {
+export interface AttachmentProps {
   title: string
   url: string
 }
