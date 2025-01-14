292064a4b4c936fcf62516a27f41640adc92fc75
leonardo <leodsmf@gmail.com>
2025-01-14 12:23:44 -0300

    comentário da resposta com autor

--- src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,4 +1,4 @@
-import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { Either, right } from '@/core/either'
 interface FetchAnswerCommentsUseCaseRequest {
@@ -8,7 +8,7 @@ interface FetchAnswerCommentsUseCaseRequest {
 type FetchAnswerCommentsUseCaseResponse = Either<
   null,
   {
-    answerComments: AnswerComment[]
+    comments: CommentWithAuthor[]
   }
 >
 export class FetchAnswerCommentsUseCase {
@@ -17,12 +17,16 @@ export class FetchAnswerCommentsUseCase {
     answerId,
     page,
   }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
-    const answerComments =
-      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
-        page,
-      })
+    const comments =
+      await this.answerCommentsRepository.findManyByAnswerIdWithAuthor(
+        answerId,
+        {
+          page,
+        },
+      )
+
     return right({
-      answerComments,
+      comments,
     })
   }
 }
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,10 +1,15 @@
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
+
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
   }
@@ -15,6 +20,34 @@ export class InMemoryAnswerCommentsRepository
       return null
     }
     return answerComment
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+        if (!author) {
+          throw new Error(
+            `Author with ID "${comment.authorId.toString()} does not exist."`,
+          )
+        }
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          author: author.name,
+        })
+      })
+    return answerComments
   }
 
   async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
